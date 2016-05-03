16400c7cecdb15367136beba2a78426c0265e582
Norman Gersdorf <norman.gersdorf@haw-hamburg.de>
2016-05-03 18:41:55 +0200

    Registration fix 0.1

--- Restopoli/src/main/java/util/Registration.java
+++ Restopoli/src/main/java/util/Registration.java
@@ -1,26 +1,47 @@
 package util;
 
 import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
- * Created by patricklanger on 12.04.16.
+ * Created by norman gersdorf on 03.05.16.
  */
 public class Registration {
 
     private static final String YELLOW_PAGES = "http://172.18.0.5:4567/services";
+    private static final String YELLOW_PAGES_NAME = "http://172.18.0.5:4567/services/of/name/";
+    
+    
+    
 
     public static String registriereService(String name, String description, String service, String uri){
-        try {
+        
+    	
+    	
+    	
+    	
+    	
+    	JSONArray test = serviceExistiert(name);
+    	
+    	if(test != null){
+    		delOldService(test);
+    	}
+    	
+    	
+    	
+    	/*try {
             HttpResponse<String> res = Unirest.post(YELLOW_PAGES)
                     .header("Content-Type", "application/json")
                     .body(createBody(name, description, service, uri))
                     .asString();
         } catch (UnirestException e) {
             e.printStackTrace();
-        }
+        }*/
 
         return null;
     }
@@ -35,16 +56,38 @@ public class Registration {
     }
     
     
-    private boolean serviceExistiert(String name){
-    	
-    	
-    	return true;
+    private static JSONArray serviceExistiert(String name){
+    	String toSearchName = YELLOW_PAGES_NAME+"group_42";
+    	JSONArray res = null;
+    	try {
+            res = Unirest.get(toSearchName).asJson().getBody().getArray();
+            
+            System.out.println("Test get List of Services");
+            
+            
+            
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+		return res;
     }
     
-    private void delOldService(){
+    private static void delOldService(JSONArray zuloeschen){
     	
+    	JSONObject elem;
+    	String [] ids;
     	
-    	
+    	 for(Object jsonEntry : zuloeschen) {
+    		 elem = (JSONObject)jsonEntry;
+    		 
+    		 ids = elem.get("services").toString().split(",");
+ 			
+    		 for(int i = 0; i<ids.length;i++){
+    			 System.out.println("service: "+ids[i].replaceAll("[^\\d]", ""));
+    		 }
+	 
+ 		}
+	
     }
     
     
