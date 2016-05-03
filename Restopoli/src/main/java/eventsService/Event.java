f1ed011f4a40a599a1f94cf0b52cab03a0397421
Patrick Langer <patrick.langer@haw-hamburg.de>
2016-05-03 10:39:51 +0200

    Events Service hinzugefügt

---
+++ Restopoli/src/main/java/eventsService/Event.java
@@ -0,0 +1,57 @@
+package eventsService;
+
+public class Event {
+
+    private String id;//not null
+    private String game;//not null
+    private String type;//not null
+    private String name;//not null
+    private String reason; //not null
+    private String resource;
+    private String player;
+    private String time;
+
+    public Event(String id, String game, String type, String name, String reason, String resource, String player, String time) {
+        this.id = id;
+        this.game = game;
+        this.type = type;
+        this.name = name;
+        this.reason = reason;
+        this.resource = resource;
+        this.player = player;
+        this.time = time;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getResource() {
+        return resource;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+}
---
+++ Restopoli/src/main/java/eventsService/Events.java
@@ -0,0 +1,53 @@
+package eventsService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Events {
+
+    private List<Event> events = new ArrayList();
+
+
+    public void addEvent(Event event){
+        events.add(event);
+    }
+
+    public void delEvent(String game, String type, String name, String reason, String resource, String player){
+        List<Integer> delIndexes = new ArrayList<Integer>();
+        for(Event e : events){
+            if(e.getGame().matches("(https://141\\.22\\.34\\.15/cnt/\\d+\\.\\d+\\.\\d+\\.\\d+/\\d\\d\\d\\d/(\\w)+(/\\w)*)|()")
+                    && e.getType().matches("([a-zA-Z_]+)|()") && e.getName().matches("(lmnp_[a-zA-Z_0-9]+)|()") && e.getReason().matches("([a-zA-Z _0-9]+)|()")
+                    && e.getResource().matches("(https://141\\.22\\.34\\.15/cnt/\\d+\\.\\d+\\.\\d+\\.\\d+/\\d\\d\\d\\d/(\\w)+(/\\w)*)|()")
+                    && e.getPlayer().matches("(https://141\\.22\\.34\\.15/cnt/\\d+\\.\\d+\\.\\d+\\.\\d+/\\d\\d\\d\\d/(\\w)+(/\\w)*)|()")){
+                delIndexes.add(events.indexOf(e));
+            }
+        }
+        for (Integer i : delIndexes){
+            events.remove(i);
+        }
+    }
+
+    public Event getEvent(String id){
+        for(Event e : events){
+            if(e.getId().equals(id)){
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public List<Event> getEvents(String game, String type, String name, String reason, String resource, String player){
+
+        List<Event> temp = new ArrayList();
+
+        for(Event e : events){
+            if(e.getGame().matches("(https://141\\.22\\.34\\.15/cnt/\\d+\\.\\d+\\.\\d+\\.\\d+/\\d\\d\\d\\d/(\\w)+(/\\w)*)|()")
+                    && e.getType().matches("([a-zA-Z_]+)|()") && e.getName().matches("(lmnp_[a-zA-Z_0-9]+)|()") && e.getReason().matches("([a-zA-Z _0-9]+)|()")
+                    && e.getResource().matches("(https://141\\.22\\.34\\.15/cnt/\\d+\\.\\d+\\.\\d+\\.\\d+/\\d\\d\\d\\d/(\\w)+(/\\w)*)|()")
+                    && e.getPlayer().matches("(https://141\\.22\\.34\\.15/cnt/\\d+\\.\\d+\\.\\d+\\.\\d+/\\d\\d\\d\\d/(\\w)+(/\\w)*)|()")){
+                temp.add(e);
+            }
+        }
+        return temp;
+    }
+}
